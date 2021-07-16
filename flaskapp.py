2933b0a10e78be0b65eb02e911d793d902dc58da
Nikita Teterin <nekitonum@gmail.com>
2021-07-16 16:09:36 +0300

    Added contents. Server now responses to GET requests from Streamlit app!

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 app = Flask(__name__)
+BASEDIR = os.getcwd()
+ENCODING = 'utf-8'
 
-@app.route('/')
+
+def getfile(filename:str):
+    global BASEDIR
+    global ENCODING
+    path = os.path.join(BASEDIR, 'content', filename)
+    if os.path.isfile(path):
+        with open(path, 'r', encoding=ENCODING) as confile:
+            imported_data = confile.read()
+        return imported_data
+    else:
+        raise FileNotFoundError
+
+@app.route('/', methods = ['GET'])
 def okay_fine():
     return 'Booba'
 
+
+@app.route('/estimation', methods = ['GET'])
+def get_estimation():
+    return getfile('estimation.json')
+
+
+@app.route('/variants', methods = ['GET'])
+def get_variants():
+    return getfile('variants.json')
+
+    
+@app.route('/model', methods = ['GET'])
+def get_model():
+    return getfile('model.json')
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
